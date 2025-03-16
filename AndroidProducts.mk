e6d83970d4d6ef4212a43e964619d1c06d62748f
Hans98 <142167463+Hans982@users.noreply.github.com>
2025-03-17 05:15:38 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -21,9 +21,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/orion_coral.mk \
-    $(LOCAL_DIR)/orion_flame.mk
+    $(LOCAL_DIR)/infinity_coral.mk \
+    $(LOCAL_DIR)/infinity_flame.mk
 
 COMMON_LUNCH_CHOICES := \
-    orion_coral-trunk_staging-userdebug \
-    orion_flame-trunk_staging-userdebug
+    infinity_coral-trunk_staging-userdebug \
+    infinity_flame-trunk_staging-userdebug
