69ad3be3cbda7472874c61382c5711642bd6eadf
Hans982 <142167463+Hans982@users.noreply.github.com>
2025-07-15 12:25:09 +0700

    Update BoardConfig-common.mk

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -17,6 +17,11 @@
 include build/make/target/board/BoardConfigMainlineCommon.mk
 include build/make/target/board/BoardConfigPixelCommon.mk
 
+ALLOW_MISSING_DEPENDENCIES := true
+TARGET_DISABLE_EPPE := true
+BUILD_BROKEN_DUP_RULES := true
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
 TARGET_BOARD_PLATFORM := msmnile
 TARGET_BOARD_INFO_FILE := device/google/coral/board-info.txt
 USES_DEVICE_GOOGLE_CORAL := true
