f1b95319509e5fbefed6ee43a76c2d87dad4d6cc
Hans982 <142167463+Hans982@users.noreply.github.com>
2025-07-30 22:19:36 +0700

    Update lineage_flame.mk
    Bringsup matrixx

--- lineage_flame.mk
+++ lineage_flame.mk
@@ -12,6 +12,14 @@ $(call inherit-product, device/google/coral/aosp_flame.mk)
 
 include device/google/coral/flame/device-lineage.mk
 
+# Inherit some common Matrixx stuff.
+WITH_GMS := true
+WITH_GMS_COMMS_SUITE := true
+TARGET_SUPPORTS_WALLEFFECT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_STOCK_AICORE := false
+
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 4
@@ -20,6 +28,7 @@ PRODUCT_NAME := lineage_flame
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="flame-user 13 TP1A.221005.002.B2 9382335 release-keys" \
