340cb76e649d0c87fc5b2d7053e9c00f709b2190
Hans982 <142167463+Hans982@users.noreply.github.com>
2025-07-30 23:22:50 +0700

    Update lineage_flame.mk

--- lineage_flame.mk
+++ lineage_flame.mk
@@ -3,6 +3,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+ALLOW_MISSING_DEPENDENCIES := true
+TARGET_DISABLE_EPPE := true
+BUILD_BROKEN_DUP_RULES := true
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
